bfbae86330bb0d066a0cdaf0d8c59fc3a93afccb
Haalon <Haalon@yandex.com>
2020-08-18 22:39:20 +0300

    Make basic Ring server

--- src/clojure_demo/core.clj
+++ src/clojure_demo/core.clj
@@ -1,7 +1,13 @@
 (ns clojure-demo.core
-  (:gen-class))
+   (:require [ring.adapter.jetty :as ring]))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (ring/run-jetty handler {:port  8080 :join? false})
+  (println "Started on localhost:8080"))
