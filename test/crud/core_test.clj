dc985055305a54621acdb420664cc17986fcca17
Haalon <haalon@yandex.com>
2020-09-05 13:30:06 +0300

    Try simple tests

--- test/crud/core_test.clj
+++ test/crud/core_test.clj
@@ -1,7 +1,15 @@
 (ns crud.core-test
   (:require [clojure.test :refer :all]
-            [crud.server.core :refer :all]))
+            [crud.server.core :refer :all]
+            [crud.server.web :as web]
+            [ring.mock.request :as mock]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 0 0))))
+
+(deftest list-test
+  (testing "List api route"
+    (let [req (mock/request :get "/api")
+          res (web/app req)]
+      (is res))))
