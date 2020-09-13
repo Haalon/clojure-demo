665f7f82a15a3adddca5fbfce1352cc0bcafaac7
Haalon <haalon@yandex.com>
2020-09-13 22:36:28 +0300

    Use figwheel-main

--- project.clj
+++ project.clj
@@ -13,22 +13,22 @@
                  [org.clojure/clojurescript "1.10.773"]
                  [org.clojure/data.json "1.0.0"]
                  [cljs-http "0.1.46"]
-                 [reagent "0.10.0"]]
+                 [reagent "0.10.0"]
+                 [re-frame "1.1.1"]
+                 [day8.re-frame/http-fx "0.2.1"]]
   :main ^:skip-aot crud.server.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-					   :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
-					   :dev {:plugins [[lein-cljsbuild "1.1.8"]
-					                   [lein-cljfmt "0.6.8"]]
-					   	     :dependencies [[reloaded.repl "0.2.4"]
-                             [ring/ring-mock "0.4.0"]]
-					   	     :source-paths ["dev"]
-              :cljsbuild {:builds [{:source-paths ["src" "dev"]
-                                    :figwheel true
-                                    :compiler {:output-to "resources/public/app.js"
-                                               :output-dir "resources/public/out"
-                                               :main "crud.client.core"
-                                               :asset-path "/out"
-                                               :optimizations :none
-                                               :recompile-dependents true
-                                               :source-map true}}]}}})
+  :source-paths ["src" "dev"]
+  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
+            "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
+            "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "hello-world.test-runner"]}
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.11"]
+                                  [reloaded.repl "0.2.4"]
+                                  [ring/ring-mock "0.4.0"]
+                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]
+                   :plugins [[lein-cljsbuild "1.1.8"]
+					                        [lein-cljfmt "0.6.8"]]
+                   
+                   :resource-paths ["target"]
+                   ;; need to add the compiled assets to the :clean-targets
+                   :clean-targets ^{:protect false} ["target"]}})
